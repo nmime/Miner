2856badbfa01570810332747761bc1586e257687
NMI <mejenkovnik@gmail.com>
2024-01-31 16:21:27 +0500

    upd

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 REPO_URL="https://github.com/nmime/Miner"
 BRANCH="main" # or your specific branch
 
+# Define config content
+
 # Install nvm (Node Version Manager)
 echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -24,12 +26,13 @@ REPO_NAME=$(basename $REPO_URL .git)
 # Change directory to the repository
 cd $REPO_NAME
 
+# Creating a config.txt file with the specified content
+echo "Creating config.txt with specified content..."
+echo "$CONFIG_CONTENT" > config.txt
+
 # Assuming there's a package.json file, install dependencies
 echo "Installing dependencies..."
 npm install
-
-npm install -g typescript
-npm install -g ts-node
 
 # Assuming there's a script named "start" in package.json
 echo "Running the application..."
