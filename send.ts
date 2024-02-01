c88639afc558503668096dc664cbe088d3be9735
NMI <mejenkovnik@gmail.com>
2024-02-01 16:14:03 +0500

    up

--- send.ts
+++ send.ts
@@ -46,7 +46,7 @@ const totalDiff = BigInt(
 );
 
 const givers = [
-  { address: "EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2", reward: 1000 },
+  /*{ address: "EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2", reward: 1000 },
   { address: "EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow", reward: 1000 },
   { address: "EQBvumwjKe7xlrjc22p2eLGT4UkdRnrmqmcEYT94J6ZCINmt", reward: 1000 },
   { address: "EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl", reward: 1000 },
@@ -55,7 +55,7 @@ const givers = [
   { address: "EQCba5q9VoYGgiGykVazOUZ49UK-1RljUeZgU6E-bW0bqF2Z", reward: 1000 },
   { address: "EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV", reward: 1000 },
   { address: "EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY", reward: 1000 },
-  { address: "EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA", reward: 1000 }, // 1000
+  { address: "EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA", reward: 1000 },*/ // 1000
 
   {
     address: "EQDcOxqaWgEhN_j6Tc4iIQNCj2dBf9AFm0S9QyouwifYo9KD",
