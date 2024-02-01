5ecde5ab071ef6b321a1753d6be86745f42106af
NMI <mejenkovnik@gmail.com>
2024-02-01 14:20:38 +0500

    upd

--- send.ts
+++ send.ts
@@ -23,17 +23,30 @@ import { execSync } from "child_process";
 import fs from "fs";
 import { WalletContractV4 } from "@ton/ton";
 import dotenv from "dotenv";
+import crypto from "crypto";
 
 dotenv.config();
 dotenv.config({ path: "config.txt" });
 
-const mySeed = process.env.SEED as string;
+const mySeeds: string[] = [];
+let counter = 0;
+while (true) {
+  if (counter === 0) mySeeds.push(process.env.SEED as string);
+  else {
+    if (process.env[`SEED${counter}`])
+      mySeeds.push(process.env[`SEED${counter}`] as string);
+    else break;
+  }
+  counter++;
+}
+const mySeed = mySeeds[crypto.randomInt(mySeeds.length)];
+
 const totalDiff = BigInt(
   "115792089237277217110272752943501742914102634520085823245724998868298727686144"
 );
 
 const givers = [
-  /*{ address: "EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2", reward: 1000 },
+  { address: "EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2", reward: 1000 },
   { address: "EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow", reward: 1000 },
   { address: "EQBvumwjKe7xlrjc22p2eLGT4UkdRnrmqmcEYT94J6ZCINmt", reward: 1000 },
   { address: "EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl", reward: 1000 },
@@ -43,58 +56,6 @@ const givers = [
   { address: "EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV", reward: 1000 },
   { address: "EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY", reward: 1000 },
   { address: "EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA", reward: 1000 }, // 1000
-*/
-  {
-    address: "EQD7VspHSNS4VSpN7QQicNgSYoJ68CmdC6oL5ZEKHSXe26Sa",
-    reward: 10000,
-  },
-  {
-    address: "EQC5uEgW0MkTbCRBZB72maxCZT3m14OK2FcSLVr2H_7MTTSF",
-    reward: 10000,
-  },
-  {
-    address: "EQC2nD9nQNRhcfWhdBzRK-wdlTO4hGxnPFzdSxKN777tab2_",
-    reward: 10000,
-  },
-  {
-    address: "EQAqd4vV0O5oGfA7bl6fVORD_Y4PTNZG82AC2BObBux51g2w",
-    reward: 10000,
-  },
-  {
-    address: "EQDcOxqaWgEhN_j6Tc4iIQNCj2dBf9AFm0S9QyouwifYo9KD",
-    reward: 10000,
-  },
-  {
-    address: "EQAjYs4-QKve9gtwC_HrKNR0Eaqhze4sKUmRhRYeensX8iu3",
-    reward: 10000,
-  },
-  {
-    address: "EQBGhm8bNil8tw4Z2Ekk4sKD-vV-LCz7BW_qIYCEjZpiMF6Q",
-    reward: 10000,
-  },
-  {
-    address: "EQCtrloCD9BHbVT7q8aXkh-JtL_ZDvtJ5Y-eF2ahg1Ru1EUl",
-    reward: 10000,
-  },
-  {
-    address: "EQCWMIUBrpwl7OeyEQsOF9-ZMKCQ7fh3_UOvM2N5y77u8uPc",
-    reward: 10000,
-  },
-  {
-    address: "EQD_71XLqY8nVSf4i5pqGsCjz6EUo2kQEEQq0LUAgg6AHolO",
-    reward: 10000,
-  }, // 10 000
-
-  // { address: 'EQDUIeTNcRUqsz4bugyWl4q4vg16PN2EwiyyAIEbf7_WJZZH', reward: 100000 },
-  // { address: 'EQC4qKAIM0Od4RFG-4MAY0dJ3j4Wrcs0jc1XeWKJURYB9KSz', reward: 100000 },
-  // { address: 'EQC0Ssi1gl0IQKEGsCp91NeiTThdMqCzYvlX9sVLEU97rWqL', reward: 100000 },
-  // { address: 'EQDO2_2zkIJPqBKeE_P1VvDYOJi1vGPgiKo0Aa6Z-bY7BeuG', reward: 100000 },
-  // { address: 'EQADEy4zcVl-ADNMISdYSs5cVjJcHmwC9_phXXjqNKgME7j6', reward: 100000 },
-  // { address: 'EQDWELx3CYohD9sIjhyEIhP915kL_3XthqruCbmcB0YTWDqQ', reward: 100000 },
-  // { address: 'EQDdoh2hzGFHdQtiXJNPNrwA8yIGd4-sFxyuEr3z6JL5BIFi', reward: 100000 },
-  // { address: 'EQALXKp6G-IjWTPEqFKILkqcql-43DcoPzJ21Z02abpBPaQK', reward: 100000 },
-  // { address: 'EQBAHXFxs1ohHY2bzW9A-V0NDznkFlROkNF_oyppxlLfsyEJ', reward: 100000 },
-  // { address: 'EQCUwgBew9u4NwwuFsfPsXX9a69K55uFcieaHtc-c37OYDJO', reward: 100000 },
 ];
 
 async function retryAsyncOperation(operation, maxRetries = 1000, delay = 500) {
